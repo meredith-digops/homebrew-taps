dc1a52ea613680f908d0af0c5184921526c99c15
David Michael <1.david.michael@gmail.com>
2018-12-07 08:58:43 -0600

    Convert to use Meredith customized saml2aws repo

--- saml2aws.rb
+++ saml2aws.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Saml2aws < Formula
-  homepage 'https://github.com/versent/saml2aws'
-  url 'https://github.com/Versent/saml2aws/releases/download/v2.10.0/saml2aws_2.10.0_darwin_amd64.tar.gz'
-  version '2.10.0'
-  sha256 '515f905ebc73b684e3e68b0a30089bf99a0b1ef792c3de24547179778b9b2792'
+  homepage 'https://github.com/meredith-digops/saml2aws/'
+  url 'https://github.com/meredith-digops/saml2aws/releases/download/v2.11.0-legacy-time-rsa/saml2aws_2.11.0_darwin_amd64.tar.gz'
+  version '2.11.0'
+  sha256 'd0bd93b26efde8f38a9974fad2f0e16dff03f0e8508008eafa36acab0aa833b3'
 
   depends_on :arch => :x86_64
 
